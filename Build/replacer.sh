eea837701fd73ef314bb26609914b16f9b699589
dgthanhan <dgthanhan@9c2e60df-0c51-0410-bbcd-610ed1c0c5c8>
2008-07-11 06:39:50 +0000

    modifying build script to generate update.rdf

--- Build/replacer.sh
+++ Build/replacer.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 echo "Processing $1..."
-sed "s/%version%/$VERSION/g;s/%author%/$AUTHOR/g;s/%name%/$NAME/g;s/%build%/$BUILD/g" < $1 > temp
+sed "s/%version%/$VERSION/g;s/%author%/$AUTHOR/g;s/%name%/$NAME/g;s/%build%/$BUILD/g;s/%xpiname%/$XPINAME/g;s/%fxminver%/$FXMINVER/g;s/%fxmaxver%/$FXMMAXVER/g;s/%xpihash%/$XPIHASH/g" < $1 > temp
 mv -f temp $1
 
