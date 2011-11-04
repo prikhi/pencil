f9d7a2a472aa21f9f64ca7bfcc036c8fe4e27b59
dgthanhan <dgthanhan@9c2e60df-0c51-0410-bbcd-610ed1c0c5c8>
2011-11-04 08:08:12 +0000

    experimental native pdf export

---
+++ branches/pencil-2.0/app/content/pencil/common/pdf.js
@@ -0,0 +1,44 @@
+var Cc = Components.classes;
+var Ci = Components.interfaces;
+var Cu = Components.utils;
+var Cr = Components.results;
+
+function test() {
+    var browser = document.getElementById("browser");
+    var printSettings = Cc["@mozilla.org/gfx/printsettings-service;1"]
+                                                            .getService(Ci.nsIPrintSettingsService)
+                                                            .newPrintSettings;
+    printSettings.printSilent = true;
+    printSettings.showPrintProgress = false;
+    printSettings.printBGImages = true;
+    printSettings.printBGColors = true;
+    printSettings.printToFile = true;
+    printSettings.toFileName = "/home/dgthanhan/Desktop/print.pdf";
+    printSettings.printFrameType = Ci.nsIPrintSettings.kFramesAsIs;
+    printSettings.outputFormat = Ci.nsIPrintSettings.kOutputFormatPDF;
+
+    //XXX we probably need a preference here, the header can be useful
+    printSettings.footerStrCenter = "";
+    printSettings.footerStrLeft     = "";
+    printSettings.footerStrRight    = "";
+    printSettings.headerStrCenter = "";
+    printSettings.headerStrLeft     = "";
+    printSettings.headerStrRight    = "";
+
+    var listener = {
+        onStateChange: function(aWebProgress, aRequest, aStateFlags, aStatus) {
+            if (aStateFlags & Ci.nsIWebProgressListener.STATE_STOP) {
+                //sendAsyncMessage("Browser:SaveAs:Return", { type: json.type, id: json.id, referrer: json.referrer });
+            }
+        },
+        onProgressChange : function(aWebProgress, aRequest, aCurSelfProgress, aMaxSelfProgress, aCurTotalProgress, aMaxTotalProgress) {},
+
+        // stubs for the nsIWebProgressListener interfaces which nsIWebBrowserPrint doesn't use.
+        onLocationChange : function() { throw "Unexpected onLocationChange"; },
+        onStatusChange     : function() { throw "Unexpected onStatusChange";     },
+        onSecurityChange : function() { throw "Unexpected onSecurityChange"; }
+    };
+
+    var webBrowserPrint = browser.contentWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIWebBrowserPrint);
+    webBrowserPrint.print(printSettings, listener);
+}
