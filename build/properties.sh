ca02a53872ef789582f65ff1607d2641c8474f09
dgthanhan <dgthanhan@9c2e60df-0c51-0410-bbcd-610ed1c0c5c8>
2012-11-28 01:05:42 +0000

    minor bug fixing

--- build/properties.sh
+++ build/properties.sh
@@ -2,10 +2,10 @@
 
 export NAME='Pencil'
 export VERSION='2.0'
-export BUILD='2'
+export BUILD='3'
 export AUTHOR='Duong Thanh An (an.duong@evolus.vn) and Contributors'
 export XPI_NAME='Pencil-'$VERSION'-'$BUILD'-fx.xpi'
 export MIN_VERSION='1.9.8'
-export MAX_VERSION='16.*'
+export MAX_VERSION='18.*'
 export UPDATE_URL="http://pencil.evolus.vn"
 export XULRUNNER_XUL="*"
