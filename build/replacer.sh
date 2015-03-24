cd3558f8ed9ddb1eeaa7ae097f6b80426a362cbc
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2015-03-24 17:01:11 -0400

    Fix Debugging Builds
    Only run the sed script to disable debug/info/warning logging if the
    DEBUG environmental variable is blank or undefined.

--- build/replacer.sh
+++ build/replacer.sh
@@ -4,5 +4,7 @@ echo "Processing $1..."
 sed "s/@VERSION@/$VERSION/g;s/@AUTHOR@/$AUTHOR/g;s/@NAME@/$NAME/g;s/@BUILD@/$BUILD/g;s/@XPI_NAME@/$XPI_NAME/g;s/@MIN_VERSION@/$MIN_VERSION/g;s/@MAX_VERSION@/$MAX_VERSION/g;s/@XPIHASH@/$XPIHASH/g;s/@XULRUNNER_XUL@/$XULRUNNER_XUL/g;" < $1 > temp
 mv -f temp $1
 
-sed -f sed-debug-script < $1 > temp
-mv -f temp $1
+if [ ! $DEBUG ]; then
+    sed -f sed-debug-script < $1 > temp
+    mv -f temp $1
+fi
