f728ad5a37fb52ac810d5d0f650de0d82c039d3f
aerlaut <aerlaut@live.com>
2015-05-02 21:59:40 +0700

    Upated build system for MacOS X 10.9.5

--- build/properties.sh
+++ build/properties.sh
@@ -2,10 +2,10 @@
 
 export NAME='Pencil'
 export VERSION='2.0.10'
-export BUILD=`date -I | sed 's/-//g'`
+export BUILD=`date +%Y%m%d`
 export AUTHOR='Pavan Rikhi(pavan.rikhi@gmail.com) and Contributors'
 export XPI_NAME='Pencil-'$VERSION'.xpi'
 export MIN_VERSION='36.0'
 export MAX_VERSION='37.*'
-export UPDATE_URL="http://pencil.evolus.vn"
+export UPDATE_URL="https://github.com/prikhi/pencil/releases"
 export XULRUNNER_XUL="*"
