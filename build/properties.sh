9cd1ba0fd3efe2b86ee4fc1932052b8fe7888817
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2015-05-28 09:04:01 -0400

    Bump Supported FF/XRE Version to 38

--- build/properties.sh
+++ build/properties.sh
@@ -6,6 +6,6 @@ export BUILD=`date +%Y%m%d`
 export AUTHOR='Pavan Rikhi(pavan.rikhi@gmail.com) and Contributors'
 export XPI_NAME='Pencil-'$VERSION'.xpi'
 export MIN_VERSION='36.0'
-export MAX_VERSION='37.*'
+export MAX_VERSION='38.*'
 export UPDATE_URL="https://github.com/prikhi/pencil/releases"
 export XULRUNNER_XUL="*"
