75573044c9c667914cdb114f4135ff3b2d11f228
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2015-05-29 04:52:48 -0400

    Always Use the Full Version with Sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -58,7 +58,7 @@ copyright = u'2015, Pencil Contributors'
 # built documents.
 #
 # The short X.Y version.
-version = '2.0'
+version = '2.0.11'
 # The full version, including alpha/beta/rc tags.
 release = '2.0.11'
 
