b5a94f95feb6f36e2b671a1d12da2cdac5654b7c
Joe Mirizio <joe@joemirizio.com>
2015-08-04 03:26:32 -0400

    Fix indent for dots logic

--- app/content/pencil/canvasHelper/canvasImpl.js
+++ app/content/pencil/canvasHelper/canvasImpl.js
@@ -1,5 +1,5 @@
-var CanvasImpl = {};
 
+var CanvasImpl = {};
 CanvasImpl.setupGrid = function () {
     if (this.gridContainer) {
         Dom.empty(this.gridContainer);
@@ -19,14 +19,14 @@ CanvasImpl.setupGrid = function () {
             var z = this.zoom ? this.zoom : 1;
             
             if (Config.get("edit.gridAppearance") === 'dots') {
-            for (var i = 1; i < this.width * z; i += grid.w * z) {
-                var line = document.createElementNS(PencilNamespaces.svg, "svg:line");
-                line.setAttribute("x1", i);
-                line.setAttribute("y1", 0);
-                line.setAttribute("x2", i);
-                line.setAttribute("y2", this.height * z);
-                line.setAttribute("style", "stroke-dasharray: 1," + (grid.h - 1) * z + ";");
-                this.gridContainer.appendChild(line);
+                for (var i = 1; i < this.width * z; i += grid.w * z) {
+                    var line = document.createElementNS(PencilNamespaces.svg, "svg:line");
+                    line.setAttribute("x1", i);
+                    line.setAttribute("y1", 0);
+                    line.setAttribute("x2", i);
+                    line.setAttribute("y2", this.height * z);
+                    line.setAttribute("style", "stroke-dasharray: 1," + (grid.h - 1) * z + ";");
+                    this.gridContainer.appendChild(line);
                 }
             } else {
                 for (var i = 1; i < this.width * z; i += grid.w * z) {
