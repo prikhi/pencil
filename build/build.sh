60a5dd11b1e1633d3338124a1e3ad85af273737c
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2015-09-21 03:47:22 -0400

    Move run_task function into build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
 . ./properties.sh
-. ./functions.sh
+
+
+run_task() {
+  task=$@
+
+  ${task}
+  result=$?
+
+  if [ "${result}" != "0" ]; then
+    echo ":: Task has failed with exit code ${result}" 1>&2
+    echo " -> ${task}"
+    exit ${result}
+  fi
+}
 
 prep() {
     rm -Rf ./Outputs/
