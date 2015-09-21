031844205885c88453c6bc3fa3ef16d4c74cac86
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2015-09-21 05:36:55 -0400

    Cleanup baseRasterizedExporter.js

--- app/content/pencil/exporter/baseRasterizedExporter.js
+++ app/content/pencil/exporter/baseRasterizedExporter.js
@@ -7,25 +7,21 @@ BaseRasterizedExporter.prototype = new BaseExporter();
 BaseRasterizedExporter.prototype.requireRasterizedData = function() {
   return true;
 };
-BaseRasterizedExporter.prototype.getRasterizedPageDestination = function(
-  baseDir) {
+BaseRasterizedExporter.prototype.getRasterizedPageDestination = function(baseDir) {
   return baseDir.clone();
 };
-
-BaseRasterizedExporter.prototype.export = function(doc, options, destFile,
-  xmlFile, callback) {
+BaseRasterizedExporter.prototype.export = function(doc, options, destFile, xmlFile, callback) {
   callback();
 };
 BaseRasterizedExporter.prototype.getWarnings = function() {
   return Util.getMessage("no.linkings.are.preserved");
 };
-BaseRasterizedExporter.prototype.fixAbsoluteRasterizedPaths = function(
-  sourceDOM, destDir) {
+BaseRasterizedExporter.prototype.fixAbsoluteRasterizedPaths = function(sourceDOM, destDir) {
   //changing rasterized path to relative
   var pathPrefix = destDir.path + DirIO.sep;
   Dom.workOn("//p:Page/@rasterized", sourceDOM, function(attr) {
     var path = attr.nodeValue;
-    if (path.indexOf(pathPrefix) == 0) {
+    if (path.indexOf(pathPrefix) === 0) {
       path = path.substring(pathPrefix.length);
       attr.nodeValue = path.replace(DirIO.sep, "/");
     }
