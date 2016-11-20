ad827dc6a49a0dcc7732cd68998781cfc00c3e0d
Pavan Rikhi <pavan.rikhi@gmail.com>
2016-11-20 10:14:23 -0500

    Support FF/XRE v53

--- build/properties.sh
+++ build/properties.sh
@@ -7,6 +7,6 @@ export AUTHOR='Pavan Rikhi(pavan.rikhi@gmail.com) and Contributors'
 export MAINTAINER='Pavan Rikhi <pavan.rikhi@gmail.com>'
 export XPI_NAME='Pencil-'$VERSION'-firefox.xpi'
 export MIN_VERSION='36.0'
-export MAX_VERSION='46.*'
+export MAX_VERSION='53.*'
 export UPDATE_URL="https://github.com/prikhi/pencil/releases"
 export XULRUNNER_XUL="*"
