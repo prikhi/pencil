2d15ff1ecd11155af4b6e6c488296d069f5c3343
Pavan Rikhi <pavan.rikhi@gmail.com>
2016-11-20 11:43:46 -0500

    Actually Fix Max Version

--- build/properties.sh
+++ build/properties.sh
@@ -7,6 +7,6 @@ export AUTHOR='Pavan Rikhi(pavan.rikhi@gmail.com) and Contributors'
 export MAINTAINER='Pavan Rikhi <pavan.rikhi@gmail.com>'
 export XPI_NAME='Pencil-'$VERSION'-firefox.xpi'
 export MIN_VERSION='4.0'
-export MAX_VERSION='52.*'
+export MAX_VERSION='52.0'
 export UPDATE_URL="https://github.com/prikhi/pencil/releases"
 export XULRUNNER_XUL="*"
